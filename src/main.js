31ef58bd5fadf92ca2b374a582353109aa493a2f
tie-jun <tie-jun@example.com>
2020-01-06 20:11:57 +0800

    配置CSS样式和rem适配环境

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载vant
 import './utils/register-vant'
+
+// 默认的HTML元素样式上提供了跨浏览器的高度一致性
+import 'normalize.css'
+
+// 配置 REM 适配
+import 'amfe-flexible'
+
+// 加载全局样式
+import './styles/index.less'
 Vue.config.productionTip = false
 
 new Vue({

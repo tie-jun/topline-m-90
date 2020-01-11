080346ff8fc71ac87e802db032ffa23c65e62d45
tie-jun <tie-jun@example.com>
2020-01-11 09:58:02 +0800

    优化token和发送token

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/Storege'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     // 存放null或者一个对象  登录成功把后端数据存到这里
-    user: JSON.parse(window.localStorage.setItem('user'))
+    user: getItem('user')
+    // user: JSON.parse(window.localStorage.setItem('user'))
   },
   // 登陆成功调用
   mutations: {
@@ -14,7 +16,8 @@ export default new Vuex.Store({
       state.user = data
 
       // 为了防止数据刷新丢失，这里还需要把数据放到本地存储
-      window.localStorage.setItem('user', JSON.stringify(state.user))
+      setItem('user', state.user)
+      // window.localStorage.setItem('user', JSON.stringify(state.user))
     }
   },
   actions: {
